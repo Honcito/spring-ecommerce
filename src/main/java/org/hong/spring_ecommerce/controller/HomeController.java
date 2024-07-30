be3a015fc471f830c77cbdf44066703f08380154
Victor Hong <vhoncito@gmail.com>
2024-07-30 23:16:27 +0100

    obtener la cantidad y buscar el producto en el HomeController para añadir al carro

--- src/main/java/org/hong/spring_ecommerce/controller/HomeController.java
+++ src/main/java/org/hong/spring_ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package org.hong.spring_ecommerce.controller;
 
+import org.hong.spring_ecommerce.model.DetalleOrden;
+import org.hong.spring_ecommerce.model.Orden;
 import org.hong.spring_ecommerce.model.Producto;
 import org.hong.spring_ecommerce.service.ProductoServiceImpl;
 import org.slf4j.Logger;
@@ -7,11 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +22,12 @@ public class HomeController {
     private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     private ProductoServiceImpl productoService;
+
+    //Para almacenar los detalles de la orden
+    private List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+    //Almacenar los datos de la orden
+    private Orden orden = new Orden();
 
     @Autowired
     public HomeController(ProductoServiceImpl productoService) {
@@ -49,7 +55,14 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart() {
+    public String addCart(@RequestParam Long id, @RequestParam Integer cantidad) {
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+
+        Optional<Producto> optionalProducto = productoService.buscarProductoPorId(id);
+        log.info("Producto añadido: {}", optionalProducto.get());
+        log.info("Cantidad: {}", cantidad);
 
         return "usuario/carrito";
     }
