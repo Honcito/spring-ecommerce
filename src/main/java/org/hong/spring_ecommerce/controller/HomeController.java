309644a48fdd3d72545f54deb5ce20cd7f6c7ce9
Victor Hong <vhoncito@gmail.com>
2024-07-30 21:50:13 +0100

    mostrar la vista del producto al usuario

--- src/main/java/org/hong/spring_ecommerce/controller/HomeController.java
+++ src/main/java/org/hong/spring_ecommerce/controller/HomeController.java
@@ -2,10 +2,13 @@ package org.hong.spring_ecommerce.controller;
 
 import org.hong.spring_ecommerce.model.Producto;
 import org.hong.spring_ecommerce.service.ProductoServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,6 +16,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     private ProductoServiceImpl productoService;
 
@@ -26,6 +31,13 @@ public class HomeController {
         List<Producto> productos = productoService.listarProductos();
         model.addAttribute("productos", productos);
         return "usuario/home";
+    }
+
+    //Método para llevarnos del botón producto a productoHome
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Long id) {
+        log.info("Id producto enviado como parámetro {}", id);
+        return "usuario/productohome";
     }
 
 }
