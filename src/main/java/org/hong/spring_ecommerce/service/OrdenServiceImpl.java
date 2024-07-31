955d70ca2d31c2d9808722e4e2b00311f73b2ff9
Victor Hong <vhoncito@gmail.com>
2024-07-31 19:35:49 +0100

    método para generar el número de la orden

--- src/main/java/org/hong/spring_ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/org/hong/spring_ecommerce/service/OrdenServiceImpl.java
@@ -5,6 +5,7 @@ import org.hong.spring_ecommerce.repository.OrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,7 +20,7 @@ public class OrdenServiceImpl implements IOrdenService{
 
     @Override
     public List<Orden> listarOrdenes() {
-        return List.of();
+        return ordenRepository.findAll();
     }
 
     @Override
@@ -35,5 +36,38 @@ public class OrdenServiceImpl implements IOrdenService{
     @Override
     public Orden guardarOrden(Orden orden) {
         return ordenRepository.save(orden);
+    }
+
+    //Generar un número de orden
+    //00010
+    public String generarNumerOrden() {
+        int numero = 0;
+        String numeroConcatenado = "";
+
+        List<Orden> ordenes = ordenRepository.findAll();
+
+        List<Integer> numeros = new ArrayList<Integer>();
+
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if (ordenes.isEmpty()) {
+            numero = 1;
+        }else {
+            //Obtiene el número mayor de la lista
+            numero = numeros.stream().max(Integer::compare).get();
+            numero ++;
+        }
+        if (numero < 10) { //0000000001, cuando llega a 10 se quita un cero de atrás, etc...
+            numeroConcatenado = "000000000" + String.valueOf(numero);
+        } else if (numero < 100) {
+            numeroConcatenado = "00000000" + String.valueOf(numero);
+        } else if (numero < 1000) {
+            numeroConcatenado = "000000" + String.valueOf(numero);
+        }else if (numero < 10000) {
+            numeroConcatenado = "0000000" + String.valueOf(numero);
+        }
+
+
+        return numeroConcatenado;
     }
 }
