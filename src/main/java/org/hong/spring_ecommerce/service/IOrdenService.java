ba5b991228008272f5f49b3adb459123a6c86794
Victor Hong <vhoncito@gmail.com>
2024-08-08 23:19:12 +0100

    funcionalidad ver detalle de compra del usuario

--- src/main/java/org/hong/spring_ecommerce/service/IOrdenService.java
+++ src/main/java/org/hong/spring_ecommerce/service/IOrdenService.java
@@ -4,10 +4,11 @@ import org.hong.spring_ecommerce.model.Orden;
 import org.hong.spring_ecommerce.model.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrdenService {
     List<Orden> listarOrdenes();
-    Orden buscarOrdenPorId(Long id);
+    Optional<Orden> buscarOrdenPorId(Long id);
     void eliminarOrden(Long id);
     Orden guardarOrden(Orden orden);
    String generarNumeroOrden();
--- src/main/java/org/hong/spring_ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/org/hong/spring_ecommerce/service/OrdenServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrdenServiceImpl implements IOrdenService{
@@ -25,8 +26,8 @@ public class OrdenServiceImpl implements IOrdenService{
     }
 
     @Override
-    public Orden buscarOrdenPorId(Long id) {
-        return null;
+    public Optional<Orden> buscarOrdenPorId(Long id) {
+        return ordenRepository.findById(id);
     }
 
     @Override
